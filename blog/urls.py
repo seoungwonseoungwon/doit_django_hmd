2400ac42973aebeb57098b689aeeb612a704a48c
seoungwonseoungwon <destiny_023@naver.com>
2023-04-07 13:19:45 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # path('<int:pk>/', views.single_post_page, name='single_post_page'),
     path('<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('', views.PostList.as_view(), name='post_list'),
+    path('category/<str:slug>/', views.category_page, name='category_filter'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -54,3 +54,70 @@ class PostDetail(DetailView):
 #             'post':post,
 #         }
 #     )
+
+
+def category_page(request, slug):
+
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list':post_list,
+            'categories':Category.objects.all(),
+            'no_category_post_count':Post.objects.filter(category=None).count,
+            'category':category,
+        }
+    )
+
+
+
+
+
+
+
+
+
+# def category_page(request, slug):
+#     context = {}
+
+#     # 선택한 슬러그의 해당하는 Category 테이블의 레코드
+#     # category = Category.objects.get(slug=slug)
+
+#     # Post 테이블에서 선택한 category의 레코드만 필터링
+#     context['post_list'] = Post.objects.filter(category=category)
+#     # Category 테이블의 목록 모두 가져옴
+#     context['categories'] = Category.objects.all()
+#     # Post 테이블에서 category 필드를 선택안한 포스트의 개수
+#     context['no_category_post_count'] = Post.objects.filter(category=None).count()
+#     # 선택한 코테고리의 레코드
+#     context['category'] = category
+#     # context['post_list'] = post_list
+
+#     if slug == 'no_category':
+#         category = '미분류'
+#         post_list = Post.objects.filter(category=None)
+#     else:
+#         category = Category.objects.get(slug=slug)
+#         post_list = Post.objects.filter(category=category)
+
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         context,
+#         {
+#             'post_list':post_list,
+#         }
+#     )
+
+
+
+
+
+
