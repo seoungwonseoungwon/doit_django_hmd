2f80c14f899da5e3d85c961cb475926b5fb376bd
seoungwonseoungwon <destiny_023@naver.com>
2023-04-07 16:25:50 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='post_list'),
     path('category/<str:slug>/', views.category_page, name='category_filter'),
     path('tag/<str:slug>/', views.tag_page),
+    path('create_post/', views.PostCreate.as_view()),
 
 ]
