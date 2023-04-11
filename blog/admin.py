b1c2844d3484508f13d5e59d366021241a05a8da
seoungwonseoungwon <destiny_023@naver.com>
2023-04-11 15:46:18 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 
 # summernote 관련 라이브러리 
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+admin.site.register(Comment)
 
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
